a8b36c3953fc1e9d74a22d93095e89724cd49138
Docente Sede AQP <Arequipa-Docente@certus.edu.pe>
2023-06-07 10:01:57 -0500

    Clase 07/06/2023

--- primerJuego/player/Player.gd
+++ primerJuego/player/Player.gd
@@ -5,7 +5,6 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	OS.center_window() #centrar la ventana
-	position = Vector2(100, 100)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,3 +28,24 @@ func _process(delta):
 #		position.y = 720
 	position.x = clamp(position.x, 0, 480)
 	position.y = clamp(position.y, 0, 720)
+	
+	if Input.is_action_pressed("ui_left") == true:
+#		get_node("AnimatedSprite").play("run")
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed("ui_right") == true:
+		get_node("AnimatedSprite").play("run")
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("ui_up") == true:
+		$AnimatedSprite.play("run")
+	elif Input.is_action_pressed("ui_down") == true:
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+
+
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("gem"):
+		area.pickup()
+		$AudioStreamPlayer.play()
